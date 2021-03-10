bb21bfbcec425e2ec5ade815481e632166e0784c
Ryogo Ikumi <carp193@yahoo.co.jp>
2021-03-10 21:33:36 +0900

    Initial commit

---
+++ app/src/main/java/sample/android/example/loupe2/MainActivity.java
@@ -0,0 +1,46 @@
+package sample.android.example.loupe2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Magnifier;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView view = findViewById(R.id.imageView2);
+        final Magnifier magnifier = new Magnifier(view);
+  //      magnifier.show(view.getWidth() / 2, view.getHeight() / 2);
+
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Fall through.
+                    case MotionEvent.ACTION_MOVE: {
+                        final int[] viewPosition = new int[2];
+                        v.getLocationOnScreen(viewPosition);
+                        magnifier.show(event.getRawX() - viewPosition[0],
+                                event.getRawY() - viewPosition[1]);
+                        break;
+                    }
+                    case MotionEvent.ACTION_CANCEL:
+                        // Fall through.
+                    case MotionEvent.ACTION_UP: {
+                        magnifier.dismiss();
+                    }
+                }
+                return true;
+            }
+        });
+
+    }
+}
